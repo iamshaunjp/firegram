89821cd99cd158ff70cc7ffc9c303a99586b457c
Shaun <shaun@thenetninja.co.uk>
2020-07-23 14:52:34 +0100

    part-3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './comps/Title';
+import UploadForm from './comps/UploadForm';
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <UploadForm />
     </div>
   );
 }
---
+++ src/comps/UploadForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ['image/png', 'image/jpeg'];
+
+  const handleChange = (e) => {
+    let selected = e.target.files[0];
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError('');
+    } else {
+      setFile(null);
+      setError('Please select an image file (png or jpg)');
+    }
+  };
+
+  return (
+    <form>
+      <label>
+        <input type="file" onChange={handleChange} />
+        <span>+</span>
+      </label>
+      <div className="output">
+        { error && <div className="error">{ error }</div>}
+        { file && <div>{ file.name }</div> }
+      </div>
+    </form>
+  );
+}
+
+export default UploadForm;
