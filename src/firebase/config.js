f4083b37dcb9c139c88d5587c9fea5d145614a25
Shaun <shaun@thenetninja.co.uk>
2020-07-23 14:23:37 +0100

    part-2

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDLmnoBvnjOQ-AaJgDw1DnVBiTETbcp8LI",
+  authDomain: "the-net-ninja-sandbox.firebaseapp.com",
+  databaseURL: "https://the-net-ninja-sandbox.firebaseio.com",
+  projectId: "the-net-ninja-sandbox",
+  storageBucket: "the-net-ninja-sandbox.appspot.com",
+  messagingSenderId: "485942827092",
+  appId: "1:485942827092:web:1811d9d8f1f5fabcd5b5c1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
