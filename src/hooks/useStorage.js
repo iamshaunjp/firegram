fa2fb0556034d0ac2c250b32886d18ffbda40cd8
Shaun <shaun@thenetninja.co.uk>
2020-07-23 16:35:12 +0100

    part-4

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { projectStorage } from '../firebase/config';
+
+const useStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    // references
+    const storageRef = projectStorage.ref(file.name);
+    
+    storageRef.put(file).on('state_changed', (snap) => {
+      let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+      setProgress(percentage);
+    }, (err) => {
+      setError(err);
+    }, async () => {
+      const url = await storageRef.getDownloadURL();
+      setUrl(url);
+    });
+  }, [file]);
+
+  return { progress, url, error };
+}
+
+export default useStorage;
